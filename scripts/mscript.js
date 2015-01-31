f03a856216c147123807e73b7bba22ede6676b23
Jimmy Gozal <jgozal@developerkit.co>
2015-01-31 01:10:14 -0500

    error diy

--- scripts/mscript.js
+++ scripts/mscript.js
@@ -110,7 +110,7 @@ $(document).ready(function(){
 	},{
 		name:'',
 		type: 'diy',
-		instructions: ["Go to <a href=www.developerkit.co/files/darkoutside.zip>www.developerkit.co/files/darkoutside.zip</a>",
+		instructions: ["Go to <a href=/files/darkoutside.zip>www.developerkit.co/files/darkoutside.zip</a>",
 "Extract file and run the script",
 "There is 1 syntax, 1 runtime, and 1 semantic error - debug the script!"
 ]
